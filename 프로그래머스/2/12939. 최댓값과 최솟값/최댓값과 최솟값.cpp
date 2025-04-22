160feec87e5b05d4107ac4810c621e54d15aa3e1
Dongs9 <65322950+Dongs9@users.noreply.github.com>
2025-04-22 11:22:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -3,39 +3,7 @@
 
 using namespace std;
 
-
 string solution(string s) {
-
-    int64_t min = 9223372036854775807;
-    int64_t max = -9223372036854775807;
-
-    string data = "";
-    for (int i = 0; i < s.length(); ++i)
-    {
-        if (32 != s[i])
-            data += s[i];
-        else
-        {
-            int64_t value = std::atoll(data.c_str());
-
-            if (value < min)
-                min = value;
-
-            if (value > max)
-                max = value;
-
-            data = "";
-        }
-    }
-
-    int64_t value = std::atoll(data.c_str());
-
-    if (value < min)
-        min = value;
-
-    if (value > max)
-        max = value;
-
-    string answer = std::to_string(min) + " " + std::to_string(max);
+    string answer = "";
     return answer;
 }
